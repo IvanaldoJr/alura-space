3cb851e845462ea5b9a0ee0d5e533d1f9ab064af
Ivanaldo Junior <juniorchagas@gazetaweb.com>
2024-04-01 18:42:38 -0300

    forms

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,4 +1,14 @@
 from django import forms
 
 class loginForms(forms.Form):
-    pass
+    nome_login = forms.CharField(
+        label="Nome de Login",
+        required=True,
+        max_length=100
+    )
+    senha = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=30,
+        widget=forms.PasswordInput()
+    )
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from usuarios.forms import loginForms
+
 def login(request):
-    return render(request, "usuarios/login.html")
+    form = loginForms()
+    return render(request, "usuarios/login.html", {"form": form})
 
 def cadastro(request):
     return render(request, "usuarios/cadastro.html")
