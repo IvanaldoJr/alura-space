28451a7597f73314176c934de31e41125e0cc61a
Ivanaldo Junior <juniorchagas@gazetaweb.com>
2024-04-02 16:25:35 -0300

    finalizando o formulario de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
-from usuarios.forms import loginForms
+from usuarios.forms import loginForms, CadastroForms
 
 def login(request):
     form = loginForms()
     return render(request, "usuarios/login.html", {"form": form})
 
 def cadastro(request):
-    return render(request, "usuarios/cadastro.html")
+    form = CadastroForms()
+    return render(request, "usuarios/cadastro.html", {"form": form})
